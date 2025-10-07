6bc51b8014cb3d6598e5876123c85bd7aa0f4e84
Erica Marigold <hi@devcomp.xyz>
2025-10-07 15:25:14 +0100

    build(nix): add macOS and Windows into possible flake systems list

--- flake.nix
+++ flake.nix
@@ -18,100 +18,109 @@
       rust-overlay,
       ...
     }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" ] (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ (import rust-overlay) ];
-        };
-
-        # --- Libraries ---
-        lib = pkgs.lib;
-        craneLib = (crane.mkLib pkgs).overrideToolchain (
-          toolchain:
-          toolchain.rust-bin.stable."1.89.0".default.override {
-            extensions = [
-              "clippy"
-              "rust-analyzer"
-              "cargo"
-              "rustc"
-              "rust-src"
-            ];
-          }
-        );
-
-        # Base arguments passed to almost all crane invocations
-        commonCraneArgs = {
-          src = lib.fileset.toSource {
-            root = ./.;
-            fileset = lib.fileset.unions ([
-              (craneLib.fileset.commonCargoSources ./.)
-              (lib.fileset.fromSource ./README.md)
-            ]);
+    flake-utils.lib.eachSystem
+      [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-arm"
+        "x86_64-windows"
+        "aarch64-windows"
+      ]
+      (
+        system:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ (import rust-overlay) ];
           };
-          nativeCheckInputs = with pkgs; [
-            lune
-            lua
-          ];
-          strictDeps = true;
-        };
 
-        # Build dependencies separately to have them cached in nix store
-        cargoArtifacts = craneLib.buildDepsOnly commonCraneArgs;
-
-        # Finally, compile the actual project
-        crate =
-          {
-            features ? [ ],
-          }:
-          craneLib.buildPackage (
-            commonCraneArgs
-            // {
-              inherit cargoArtifacts;
-              doChecks = false;
+          # --- Libraries ---
+          lib = pkgs.lib;
+          craneLib = (crane.mkLib pkgs).overrideToolchain (
+            toolchain:
+            toolchain.rust-bin.stable."1.89.0".default.override {
+              extensions = [
+                "clippy"
+                "rust-analyzer"
+                "cargo"
+                "rustc"
+                "rust-src"
+              ];
             }
           );
 
-        croshet = pkgs.callPackage crate { };
-      in
-      {
-        apps.default = flake-utils.lib.mkApp { drv = croshet; };
-        packages = {
-          default = croshet;
-          inherit croshet;
-        };
+          # Base arguments passed to almost all crane invocations
+          commonCraneArgs = {
+            src = lib.fileset.toSource {
+              root = ./.;
+              fileset = lib.fileset.unions ([
+                (craneLib.fileset.commonCargoSources ./.)
+                (lib.fileset.fromSource ./README.md)
+              ]);
+            };
+            nativeCheckInputs = with pkgs; [
+              lune
+              lua
+            ];
+            strictDeps = true;
+          };
 
-        checks = {
-          inherit croshet;
-          formatting = pkgs.runCommandLocal "treefmt-check" { buildInputs = [ pkgs.nixfmt-tree ]; } ''
-            set -euo pipefail
-            cp -r ${./.} workdir
-            chmod -R +w workdir/
-            treefmt --ci --tree-root workdir/
-            touch $out
-          '';
-        };
+          # Build dependencies separately to have them cached in nix store
+          cargoArtifacts = craneLib.buildDepsOnly commonCraneArgs;
 
-        formatter = pkgs.nixfmt-tree;
-        devShells.default = craneLib.devShell {
-          inputsFrom = [ croshet ];
-          checks = self.checks.${system};
-          packages = with pkgs; [
-            git
-            nixfmt-tree
-            lua
-            lune
-          ];
+          # Finally, compile the actual project
+          crate =
+            {
+              features ? [ ],
+            }:
+            craneLib.buildPackage (
+              commonCraneArgs
+              // {
+                inherit cargoArtifacts;
+                doChecks = false;
+              }
+            );
 
-          shellHook = ''
-            # Use host's default shell to make it more homely
-            if [[ $- == *i* ]]; then
-              export SHELL=$(getent passwd $USER | cut -d: -f7)
-              exec $SHELL
-            fi
-          '';
-        };
-      }
-    );
+          croshet = pkgs.callPackage crate { };
+        in
+        {
+          apps.default = flake-utils.lib.mkApp { drv = croshet; };
+          packages = {
+            default = croshet;
+            inherit croshet;
+          };
+
+          checks = {
+            inherit croshet;
+            formatting = pkgs.runCommandLocal "treefmt-check" { buildInputs = [ pkgs.nixfmt-tree ]; } ''
+              set -euo pipefail
+              cp -r ${./.} workdir
+              chmod -R +w workdir/
+              treefmt --ci --tree-root workdir/
+              touch $out
+            '';
+          };
+
+          formatter = pkgs.nixfmt-tree;
+          devShells.default = craneLib.devShell {
+            inputsFrom = [ croshet ];
+            checks = self.checks.${system};
+            packages = with pkgs; [
+              git
+              nixfmt-tree
+              lua
+              lune
+            ];
+
+            shellHook = ''
+              # Use host's default shell to make it more homely
+              if [[ $- == *i* ]]; then
+                export SHELL=$(getent passwd $USER | cut -d: -f7)
+                exec $SHELL
+              fi
+            '';
+          };
+        }
+      );
 }
